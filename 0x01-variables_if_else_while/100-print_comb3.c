b5be992d7c856ad214b638e2c75a0dbc2cecdcd4
noahire <noahire@gmail.com>
2022-10-16 20:57:18 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	int b = 1;
+
+	while
+		(a < 10) {
+			while
+				(b < 10) {
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+				b++;
+				}
+			a++;
+			b = a + 1;
+		}
+	putchar('\n');
+	return (0);
+}
