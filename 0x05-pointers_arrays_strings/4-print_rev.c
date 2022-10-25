9664208007fbe1efd674646f270feb186ce3ccd4
noahire <noahire@gmail.com>
2022-10-25 01:18:01 +0000

    more solutions

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,14 @@
 void print_rev(char *s)
 {
 	int i;
+	int n;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
